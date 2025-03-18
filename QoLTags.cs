46905593144426443627a86555f90a649184b88b
M 4 <m4vr30@gmail.com>
2025-03-18 13:11:09 +0000

    Initial commit

---
+++ QoLTags.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using GorillaLocomotion;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BananaWatch.QoLTags
+{
+    [BepInPlugin("com.M4.QoLTags", "QoLTags", "1.0.0")]
+    public class QoLTag : BaseUnityPlugin
+    {
+        private static AssetBundle qoltag;
+        private static GameObject qoltagprefab;
+        private static bool nametagEnabled = false; 
+        private static bool showPlatform = true;
+        private static bool namesshown = true;
+
+        private static bool initited;
+        private void Update()
+        {
+            if (!initited && Player.Instance != null)
+            {
+                qoltag = LoadAssetBundle("BananaWatch.Resources.qoltag");
+                initited = true;
+            }
+
+            if (!PhotonNetwork.InRoom) return;
+
+            foreach (VRRig vrrgi in GorillaParent.instance.vrrigs)
+            {
+                if (vrrgi.isOfflineVRRig) continue;
+                if (nametagEnabled)
+                {
+                    qoltagprefab = qoltag.LoadAsset<GameObject>("QoLTag");
+                    GameObject QoLTag = Instantiate(qoltagprefab);
+                    QoLTag.transform.SetParent(vrrgi.transform);
+                    QoLTag.transform.localPosition = new Vector3(0f, 0.6f, 0f);
+                    QoLTag.transform.localScale = Vector3.one * 0.8f;
+                    Quaternion currotaitotn = QoLTag.transform.rotation;
+                    Vector3 headdir = Player.Instance.headCollider.transform.position - QoLTag.transform.position;
+                    float yrot = Mathf.Atan2(headdir.x, headdir.z) * Mathf.Rad2Deg;
+                    QoLTag.transform.rotation = Quaternion.Euler(currotaitotn.eulerAngles.x, yrot, currotaitotn.eulerAngles.z);
+
+                    if (namesshown)
+                    {
+                        Text qoltagText = QoLTag.GetComponentInChildren<Text>();
+                        qoltagText.text = $"{vrrgi.OwningNetPlayer.NickName}";
+                        qoltagText.color = vrrgi.mainSkin.material.color;
+                    }
+                    else
+                    {
+                        Text qoltagText = QoLTag.GetComponentInChildren<Text>();
+                        qoltagText.text = " ";
+                    }
+
+                    if (showPlatform)
+                    {
+                        bool steamcosm = vrrgi.concatStringOfCosmeticsAllowed.Contains("FIRST LOGIN");
+                        Transform QoLCanvas = QoLTag.transform.Find("Canvas");
+                        QoLCanvas.Find(steamcosm ? "PlatformSteam" : "PlatformMeta").gameObject.SetActive(true);
+                        QoLCanvas.Find(steamcosm ? "PlatformMeta" : "PlatformSteam").gameObject.SetActive(false);
+                    }
+
+                    GameObject.Destroy(QoLTag, Time.deltaTime);
+                }
+            }
+        }
+        public static void ToggleNametag(bool enabled)
+        {
+            nametagEnabled = enabled;
+        }
+
+        public static void TogglePlatform(bool enabled)
+        {
+            showPlatform = enabled;
+        }
+
+        public static void ToggleNames(bool enabled)
+        {
+            namesshown = enabled;
+        }
+
+        private static string FormatPath(string path)
+        {
+            return path.Replace("/", ".").Replace("\\", ".");
+        }
+
+        public static AssetBundle LoadAssetBundle(string path)
+        {
+            path = FormatPath(path);
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path);
+
+            if (stream == null)
+            {
+                return null;
+            }
+
+            AssetBundle bundle = AssetBundle.LoadFromStream(stream);
+            if (bundle == null)
+            {
+                return null;
+            }
+
+            stream.Close();
+            return bundle;
+        }
+    }
+}
---
+++ QoLTagsPage.cs
@@ -0,0 +1,143 @@
+﻿using BananaWatch.Pages;
+using BepInEx;
+using System;
+
+namespace BananaWatch.QoLTags
+{
+    [BepInPlugin("M4.QoLTags", "QoLTags", "1.0.0")]
+    public class QoLTags : BaseUnityPlugin { }
+
+    public class QoLTagsPage : BananaWatchPage
+    {
+        public override string MMHeader => "<color=#ffff00><i>QoLTags</i></color>";
+        public override bool MMDisplay => true;
+
+        private SelectionHandler selectionHandler = new SelectionHandler();
+
+        private bool qoltagtoggle = false;
+        private string displayOption = "NAMES + PLATFORMS";
+
+        public override void PageOpened()
+        {
+            selectionHandler.maxIndex = 1;
+            selectionHandler.currentIndex = 0;
+        }
+
+        public string SelectionArrow(int index, string text)
+            => selectionHandler.currentIndex == index ? $"<color=#FD0000>></color> {text}" : $" {text}";
+
+        public override string RenderScreenContent()
+        {
+            string header = "<color=#ffff00>==</color> <color=#000000>Q</color><color=#1A1A1A>o</color><color=#333333>L</color><color=#4D4D4D>T</color><color=#666666>a</color><color=#808080>g</color><color=#999999>s</color> <color=#ffff00>==</color>\r\n\r\n";
+            header += $"Toggle QoLTags: \r\n <color={(qoltagtoggle ? "green" : "red")}>{(qoltagtoggle ? "Enabled" : "Disabled")}</color>\r\n";
+            header += "Display Options: \r\n";
+            header += $"< {displayOption} >\r\n";
+
+            return header;
+        }
+
+        public override void ButtonPressed(BananaWatchButton buttonType)
+        {
+            switch (buttonType)
+            {
+                case BananaWatchButton.Up:
+                    selectionHandler.MoveSelectionUp();
+                    break;
+                case BananaWatchButton.Down:
+                    selectionHandler.MoveSelectionDown();
+                    break;
+                case BananaWatchButton.Enter:
+                    HandleSelection();
+                    break;
+                case BananaWatchButton.Left:
+                    if (selectionHandler.currentIndex == 0)
+                    {
+                        ChangeDisplayOption(-1);
+                    }
+                    break;
+                case BananaWatchButton.Right:
+                    if (selectionHandler.currentIndex == 0) 
+                    {
+                        ChangeDisplayOption(1);
+                    }
+                    break;
+                case BananaWatchButton.Back:
+                    NavigateToMM();
+                    break;
+            }
+        }
+
+        private void HandleSelection()
+        {
+            if (selectionHandler.currentIndex == 0)
+            {
+                qoltagtoggle = !qoltagtoggle;
+                QoLTagHandler.ToggleNametag(qoltagtoggle);
+            }
+        }
+
+        private void ChangeDisplayOption(int direction)
+        {
+            string[] options = { "NAMES", "PLATFORMS", "NAMES + PLATFORMS" };
+            int currentIndex = Array.IndexOf(options, displayOption);
+            int newIndex = (currentIndex + direction + options.Length) % options.Length;
+            displayOption = options[newIndex];
+
+            // Toggle corresponding features based on the display option
+            if (displayOption == "NAMES")
+            {
+                QoLTagHandler.ToggleNames(true);
+                QoLTagHandler.TogglePlatform(false);
+            }
+            else if (displayOption == "PLATFORMS")
+            {
+                QoLTagHandler.ToggleNames(false);
+                QoLTagHandler.TogglePlatform(true);
+            }
+            else if (displayOption == "NAMES + PLATFORMS")
+            {
+                QoLTagHandler.ToggleNames(true);
+                QoLTagHandler.TogglePlatform(true);
+            }
+        }
+    }
+
+    public static class QoLTagHandler
+    {
+        public static void ToggleNametag(bool enabled)
+        {
+            if (enabled)
+            {
+                QoLTag.ToggleNametag(true);
+            }
+            else
+            {
+                QoLTag.ToggleNametag(false);
+            }
+        }
+
+        public static void TogglePlatform(bool enabled)
+        {
+            if (enabled)
+            {
+                QoLTag.TogglePlatform(true);
+            }
+            else
+            {
+                QoLTag.TogglePlatform(false);
+            }
+        }
+
+        public static void ToggleNames(bool enabled)
+        {
+            if (enabled)
+            {
+                QoLTag.ToggleNames(true);
+            }
+            else
+            {
+                QoLTag.ToggleNames(false);
+            }
+        }
+    }
+}
